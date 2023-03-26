992b95258c91726b38a3ff843b3312d0c9cf4926
Mishaal <mishaal.20210822@iit.ac.lk>
2023-03-26 17:05:55 +0530

    added code to update git stats

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
 ```python
                     ~?JJ??!^:.                          mishaal.rifath
-                  ?B@@@@@@@@&#P!.                       ————————————
+                  ?B@@@@@@@@&AP!.                       ————————————
                  P@@@@@@@@@@@@@@G.                      uptime: 20 years, 7 months, 26 days"
-                ?@@@@@@@@@#BPB@@&^                      host: Informatics Institute of Technology | Robert Gordon University
+                ?@@@@@@@@@XBPB@@&^                      host: Informatics Institute of Technology | Robert Gordon University
                 Y@&GG5B@5^!YPB&&?                       kernel: Artificial Intelligence & Data Science
                 ^@@Y5!.?. .J&&5~                        
                  G@B5PJYJ?YJ^^:.                        skills: Machine Learning, Data Handling, Problem Solving
                  G@Y:Y@@@@&GYBBJ7.                      languages.programming: Python, Java, R, JS
        .::.  .?7!?:5 7@@@@@@@P^~J~7.                    languages.real: English, Arabic, Sinhala
       ~!~~7J7~Y#&BY! ?@@Y??J7.!!~#5!5?                  
-     YY:  ^7YY~!!PBBBB&#^      .7GG@@@7:.               contact
+     YY:  ^7YY~!!PBBBB&L^      .7GG@@@7:.               contact
     ~&@#~  .~!?57 ^~Y@@&&7:       ~J5G&&7 ..            ————————————
-   :#@&&G?!:.^~G@!   ^5YBG?:~^        ~@#7:.            email: mishaal.20210822@iit.ac.lk
+   :$@&&G?!:.^~G@!   ^5YBG?:~^        ~@%7:.            email: mishaal.20210822@iit.ac.lk
    ?@@@&&P7^^::YB7     ~P?!7GYG5.     .?BG.             LinkedIn: in/mishaal-rifath
-   7@@@@P~.....7BYYGBG5JJJ5755GBP! .    .P#^      
-   !@@@@Y:      7&&BBGPPGG!:7B5GJ :?J~:   G#^           GitHub stats:
-   ?@@@@@&J      !!????J5YY7..:   ^#@@&#Y 7@#JYJ7^      ————————————
-   7@@@@@@@5~         :JJ~.  ..  .G@@@@57J#@@@@@@@      Repos: x | Commits: y | Stars: z
-   !@@@&@@@@&Y!~^:.         .:  .G@@@@@#@@@@@@@@@@      Lines of code written: i
+   7@@@@P~.....7BYYGBG5JJJ5755GBP! .    .P%^      
+   !@@@@Y:      7&&BBGPPGG!:7B5GJ :?J~:   G%^           GitHub stats:
+   ?@@@@@&J      !!????J5YY7..:   ^&@@&&Y 7@&JYJ7^      ————————————
+   7@@@@@@@5~         :JJ~.  ..  .G@@@@57J&@@@@@@@      Repos: x | Commits: y | Stars: z
+   !@@@&@@@@&Y!~^:.         .:  .G@@@@@!@@@@@@@@@@      Lines of code written: i
 ```
--- summary.py
+++ summary.py
@@ -1,36 +1,38 @@
 from datetime import datetime
 from dateutil import relativedelta
+import requests
+from config import token
 
-date1 = datetime(2002, 7, 28)
+birthDate = datetime(2002, 7, 28)
 now = datetime.today()
-date2 = datetime(now.year, now.month, now.day)
+currentDate = datetime(now.year, now.month, now.day)
 
-diff = relativedelta.relativedelta(date2, date1)
+diff = relativedelta.relativedelta(currentDate, birthDate)
 
 years = diff.years
 months = diff.months
 days = diff.days
 
 
-def dailyreadme():
+def updateUptime():
     if years > 85:
-        return 'null. I am dead. Go out there and enjoy life."'
+        return 'null. I am dead. Go out there and enjoy life.'
     elif days == 1:
         if months == 1:
-            return '{} years, {} month, {} day"'.format(years, months, days)
+            return '{} years, {} month, {} day'.format(years, months, days)
         else:
-            return '{} years, {} months, {} day"'.format(years, months, days)
+            return '{} years, {} months, {} day'.format(years, months, days)
     elif months == 1:
-        return '{} years, {} month, {} days"'.format(years, months, days)
+        return '{} years, {} month, {} days'.format(years, months, days)
     elif days == 0:
         if months == 0:
             return '{} years"'.format(years)
         else:
-            return '{} years, {} months"'.format(years, months, days)
+            return '{} years, {} months'.format(years, months, days)
     elif months == 0:
         return '{} years, {} days"'.format(years, days)
     else:
-        return '{} years, {} months, {} days"'.format(years, months, days)
+        return '{} years, {} months, {} days'.format(years, months, days)
 
 
 def converttuple(tup):
@@ -38,12 +40,70 @@ def converttuple(tup):
     return con
 
 
+def getGitStats(token):
+    headers = {'Authorization': f'token {token}'}
+    query = """
+    query {
+      viewer {
+        repositories {
+          totalCount
+          edges {
+            node {
+              stargazers {
+                totalCount
+              }
+              languages {
+                edges {
+                  node {
+                    name
+                  }
+                }
+              }
+              object(expression: "master") {
+                ... on Commit {
+                  history {
+                    totalCount
+                  }
+                }
+              }
+              ref(qualifiedName: "master") {
+                target {
+                  ... on Commit {
+                    history {
+                      totalCount
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    """
+    response = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
+    data = response.json()['data']['viewer']['repositories']['edges']
+    repo_count = len(data)
+    star_count = sum([d['node']['stargazers']['totalCount'] for d in data])
+    code_count = sum([d['node']['object']['history']['totalCount'] for d in data])
+    commit_count = sum([d['node']['ref']['target']['history']['totalCount'] for d in data])
+    return repo_count, star_count, code_count, commit_count
+
+
 def readmeoverwrite():
+    repo_count, star_count, code_count, commit_count = getGitStats(token)
     with open("README.md", "r") as file:
         data = file.readlines()
-        line4 = ('                 P@@@@@@@@@@@@@@G.                      uptime: ', dailyreadme(), "\n")
-    tup2str = converttuple(line4)
-    data[3] = tup2str
+        line4 = ('                 P@@@@@@@@@@@@@@G.                      uptime: ', updateUptime(), "\n")
+        line19 = ('   7@@@@@@@5~         :JJ~.  ..  .G@@@@57J&@@@@@@@      Repos: {} | Commits: {} | Stars: {} \n'.format(repo_count, commit_count, star_count))
+        line20 = ('   !@@@&@@@@&Y!~^:.         .:  .G@@@@@!@@@@@@@@@@      Lines of code written: {}'.format(code_count))
+
+    tup2str4 = converttuple(line4)
+    tup2str19 = converttuple(line19)
+    tup2str20 = converttuple(line20)
+    data[3] = tup2str4
+    data[18] = tup2str19
+    data[19] = tup2str20
 
     with open('README.md', 'w') as file:
         file.writelines(data)
